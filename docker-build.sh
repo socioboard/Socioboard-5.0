07d6a5f187488ad6f9f8753d1648d17db943a5b1
vaughngx4 <72740493+vaughngx4@users.noreply.github.com>
2022-07-03 22:41:07 +0200

    Update docker-build.sh
    - Commented out local registry caching
    - Added normal build
    - Added notary comments

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+# make temporary copy of source code
 cp -r ./socioboard-api ./docker/socioboard/
 cp -r ./socioboard-web-php ./docker/socioboard/
 cd docker
-docker pull localhost:5051/socioboard/socioboard:latest
-docker build --cache-from localhost:5051/socioboard/socioboard:latest -t socioboard/socioboard ./socioboard
-docker rmi localhost:5051/socioboard/socioboard
-docker tag socioboard/socioboard:latest localhost:5051/socioboard/socioboard:latest
-docker push localhost:5051/socioboard/socioboard:latest
+
+# pull cached image
+#docker pull localhost:5051/socioboard/socioboard:latest
+
+# build without caching
+docker build -t socioboard/socioboard ./socioboard
+
+# build with caching
+#docker build --cache-from localhost:5051/socioboard/socioboard:latest -t socioboard/socioboard ./socioboard
+#docker rmi localhost:5051/socioboard/socioboard
+#docker tag socioboard/socioboard:latest localhost:5051/socioboard/socioboard:latest
+#docker push localhost:5051/socioboard/socioboard:latest
+
+# create docker network
 docker network create --subnet=172.16.32.0/16 --gateway=172.16.32.1 scb-net
+
+# remove temporary copy of source code
 rm -rf ./socioboard/socioboard-api ./socioboard/socioboard-web-php
